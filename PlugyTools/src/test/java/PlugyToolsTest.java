064acf8723e242f4841541025a36a3c44d2dce5c
Tedris <vanloone@gmail.com>
2019-06-25 08:00:13 -0400

    Fixing project setup

--- PlugyTools/src/test/java/PlugyToolsTest.java
+++ PlugyTools/src/test/java/PlugyToolsTest.java
@@ -1,15 +1,13 @@
-package test;
-
 import org.junit.Test;
 
 import plugytools.PlugyTools;
 
-class PlugyToolsTest {
+public class PlugyToolsTest {
 	
 	PlugyTools plugyTools;
 
 	@Test
-	void testGetItemFromBinaryString() {
+	public void testGetItemFromBinaryString() {
 		String itemHex = "4A4D100080006500723AD626030233B85183F105C8000026004C006C00EC84500AA115422D84A67459D898524C8199E78382C27F";
 		plugyTools = new PlugyTools();
 		byte[] itemArray = null;
